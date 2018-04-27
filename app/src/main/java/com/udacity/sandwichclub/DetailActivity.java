adaabadded960cc026ca7d18fc7415ccfa6809f6
vmalkoti <vijay.malkoti@gmail.com>
2018-04-27 08:21:35 -0700

    updated layout and added code to jsonutil,detailActivity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -35,17 +37,18 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        String jsonText = sandwiches[position];
+        Sandwich sandwich = JsonUtils.parseSandwichJson(jsonText);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .error(R.mipmap.no_image_available) // using an image with text (created in Pinta)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
@@ -56,7 +59,25 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     * Populate activity views with sandwich details
+     * @param sandwich Sandwich object with details for the activity
+     */
+    private void populateUI(Sandwich sandwich) {
+        final String DELIMITER = ", ";
+
+        String knownAsValue = TextUtils.join(DELIMITER, sandwich.getAlsoKnownAs());
+        String ingredientsValue = TextUtils.join(DELIMITER, sandwich.getIngredients());
+
+        TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        TextView originTv = findViewById(R.id.origin_tv);
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+
+        alsoKnownAsTv.setText(knownAsValue);
+        originTv.setText(sandwich.getPlaceOfOrigin());
+        descriptionTv.setText(sandwich.getDescription());
+        ingredientsTv.setText(ingredientsValue);
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,67 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        final String NAME = "name";
+        final String MAIN_NAME = "mainName";
+        final String ALSO_KNOWN_AS = "alsoKnownAs";
+        final String ORIGIN = "placeOfOrigin";
+        final String DESCRIPTION = "description";
+        final String IMAGE = "image";
+        final String INGREDIENTS = "ingredients";
+
+        Sandwich sandwich = null;
+
+        try {
+            // can't use try-with; needs API 19
+            JSONObject sandwichJson = new JSONObject(json);
+            JSONObject nameJson = sandwichJson.getJSONObject(NAME);
+
+            final String sandwichMainName = nameJson.getString(MAIN_NAME);
+            final List<String> nickNamesList = getList(nameJson.getJSONArray(ALSO_KNOWN_AS));
+            final String originCountry = sandwichJson.getString(ORIGIN);
+            final String sandwichDescription = sandwichJson.getString(DESCRIPTION);
+            final String imageUrl = sandwichJson.getString(IMAGE);
+            final List<String> sandwichIngredients = getList(sandwichJson.getJSONArray(INGREDIENTS));
+
+            sandwich = new Sandwich(sandwichMainName,
+                    nickNamesList,
+                    originCountry,
+                    sandwichDescription,
+                    imageUrl,
+                    sandwichIngredients);
+
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+        }
+
+        return sandwich;
+    }
+
+    /**
+     * Get a List of string values from a JSONArray
+     * @param array JSONArray to be used to populate List
+     * @return List of String values read from JSONArray
+     */
+    private static List<String> getList(JSONArray array) {
+        List<String> list = new ArrayList<>();
+
+        for(int i=0; i<array.length(); i++) {
+            try {
+                list.add(array.getString(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 }
