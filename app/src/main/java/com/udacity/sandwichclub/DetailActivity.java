325a009e528b7e8a91033ed09b51b9693cf71271
vmalkoti <vijay.malkoti@gmail.com>
2018-05-01 20:49:16 -0700

    Databinding updates

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
@@ -22,11 +24,13 @@ public class DetailActivity extends AppCompatActivity {
     private static final String DEFAULT_INGREDIENTS = "Ingredients not available";
     private static final String DEFAULT_ORIGIN = "Not known";
 
+    ActivityDetailBinding detailBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        //setContentView(R.layout.activity_detail);
+        detailBinding = DataBindingUtil.setContentView(this , R.layout.activity_detail);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -77,26 +81,27 @@ public class DetailActivity extends AppCompatActivity {
                 DEFAULT_INGREDIENTS: TextUtils.join(DELIMITER, sandwich.getIngredients());
         String descriptionValue = sandwich.getDescription();
 
+        /*
         ImageView ingredientsIv = findViewById(R.id.image_iv);
-
         TextView mainNameTv = findViewById(R.id.main_name_tv);
         TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
         TextView originTv = findViewById(R.id.origin_tv);
         TextView descriptionTv = findViewById(R.id.description_tv);
         TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        */
+
+        setTitle(mainNameValue);
 
         Picasso.with(this)
                 .load(imgUrlValue)
                 .error(R.mipmap.no_image_available) // using an image with text (created in Pinta)
-                .into(ingredientsIv);
+                .into(detailBinding.imageIv);
 
-        setTitle(mainNameValue);
-
-        mainNameTv.setText(mainNameValue);
-        alsoKnownAsTv.setText(knownAsValue);
-        originTv.setText(originValue);
-        descriptionTv.setText(descriptionValue);
-        ingredientsTv.setText(ingredientsValue);
+        detailBinding.mainNameTv.setText(mainNameValue);
+        detailBinding.sandwichInfo.alsoKnownTv.setText(knownAsValue);
+        detailBinding.sandwichInfo.originTv.setText(originValue);
+        detailBinding.sandwichInfo.descriptionTv.setText(descriptionValue);
+        detailBinding.sandwichInfo.ingredientsTv.setText(ingredientsValue);
 
     }
 }
